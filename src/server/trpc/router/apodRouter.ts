57de75893242ada25ab2d3ed5178fa44a0d3dd0e
Robin Meischke <robin@nierhain.de>
2022-10-25 22:21:31 +0200

    add update apod trpc endpoint

--- src/server/trpc/router/apodRouter.ts
+++ src/server/trpc/router/apodRouter.ts
@@ -3,11 +3,13 @@ import { z } from "zod";
 import { TRPCError } from "@trpc/server";
 import { env } from "../../../env/server.mjs";
 import { isAdmin } from "./_app";
+import dayjs from "dayjs";
+import { Apod, ApodType } from "@prisma/client";
 
 export const apodRouter = router({
   populateDb: publicProcedure
     .input(z.string().length(32))
-    .mutation(({ ctx, input }) => {
+    .mutation(async ({ ctx, input }) => {
       if (!isAdmin(input)) {
         throw new TRPCError({
           code: "UNAUTHORIZED",
@@ -15,9 +17,26 @@ export const apodRouter = router({
             "You are not authorized to use this route. Apply the correct key for admin actions.",
         });
       }
-      fetch(
-        `https://api.nasa.gov/planetary/apod?api_key=${env.NASA_KEY}&start_date=`
+      let today = dayjs();
+      let lastYear = dayjs().subtract(1, "year");
+
+      let res = await fetch(
+        `https://api.nasa.gov/planetary/apod?api_key=${env.NASA_KEY}&start_date=${lastYear}&end_date=${today}`
       );
+      let data = (await res.json()) as ApodResponse[];
+      data.forEach((apod) => {
+        ctx.prisma.apod.create({
+          data: {
+            author: apod.copyright,
+            createdAt: apod.date,
+            description: apod.explanation,
+            url: apod.url,
+            type: apod.media_type === "image" ? ApodType.IMAGE : ApodType.VIDEO,
+            title: apod.title,
+            updatedAt: apod.date,
+          },
+        });
+      });
     }),
 
   getToday: publicProcedure.query(({ ctx }) => {
@@ -35,3 +54,13 @@ export const apodRouter = router({
     });
   }),
 });
+
+export interface ApodResponse {
+  copyright: string;
+  date: Date;
+  explanation: string;
+  media_type: "image" | "video";
+  service_version: string;
+  title: string;
+  url: string;
+}
